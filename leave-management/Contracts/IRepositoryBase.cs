0fc6d2c5058bd6a5a3646a104826d8502b58039c
pentondb <derek.penton@gmail.com>
2020-01-19 10:46:05 -0600

    Add project files.

---
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        ICollection<T> FindAll();
+        T Find(int id);
+        bool Create(T entity);
+        bool Update(T entity);
+        bool Delete(T entity);
+        bool Save();
+    }
+}
